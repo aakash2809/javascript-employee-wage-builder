d77c4f04904441b96633a84953b5ce113e9a0bad
aakash2809 <aakashrajak289@gmail.com>
2020-12-10 09:42:03 +0530

    Add : Part Time Employee Wage

--- EmpWageBuilder.js
+++ EmpWageBuilder.js
@@ -1,7 +1,8 @@
 
-var IS_FULL_TIME = 1;  //constatnt
-var WAGE_PER_HOUR = 20;  //wage per hour
-var FULL_DAY_HOUR = 8;  //Number of fixed Hours perday
+var IS_PRESENT = 1; //default present status
+var WAGE_PER_HOUR = 20; //wage per hour
+var FULL_DAY_HOUR = 8; //Number of fixed Hours for full day
+var PART_TIME_HOUR = 4; //Number of fixed Hours for half day
 var calculateDailyWage = 0;
 
 console.log("           Welcome to EmployeeWage Computation System                 ");
@@ -9,7 +10,7 @@ console.log("-------------------------------------------------------------------
 
 var empCheck = Math.floor(Math.random() * 10) % 2; //Employee status computation
 
-if (empCheck == IS_FULL_TIME) {
+if (empCheck == IS_PRESENT) {
     console.log("Employee is Present");
 } else {
     console.log("Employee is not Present");
@@ -17,4 +18,8 @@ if (empCheck == IS_FULL_TIME) {
 
 calculateDailyWage = (FULL_DAY_HOUR * WAGE_PER_HOUR); //daily full day wage camputation
 console.log("Employee daily Full day wage - " + calculateDailyWage);
+
+calculateDailyWage = 0;                             // assigning zero for computation  wage for part time
+calculateDailyWage = (PART_TIME_HOUR * WAGE_PER_HOUR); //daily part time wage camputation
+console.log("Employee daily Part time day wage - " + calculateDailyWage);
 
