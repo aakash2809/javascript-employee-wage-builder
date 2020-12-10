8b8eb586a127e3e3b4cd823eb881459ddac6cac1
aakash2809 <aakashrajak289@gmail.com>
2020-12-10 09:05:57 +0530

    Add : Employee Attandance Status

--- EmpWageBuilder.js
+++ EmpWageBuilder.js
@@ -1,3 +1,13 @@
+
+var IS_FULL_TIME = 1;  //constatnt
+
 console.log("           Welcome to EmployeeWage Computation System                 ");
 console.log("----------------------------------------------------------------------");
 
+var empCheck = Math.floor(Math.random() * 10) % 2; //Employee status computation
+
+if (empCheck == IS_FULL_TIME) {
+    console.log("Employee is Present");
+} else {
+    console.log("Employee is not Present");
+}
