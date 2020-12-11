b9298dc228f4be34d0e781562760699ae5b54452
aakash2809 <aakashrajak289@gmail.com>
2020-12-12 01:16:24 +0530

    Added : Seprate method to caclulate employee wage

--- EmpWageBuilder.js
+++ EmpWageBuilder.js
@@ -6,10 +6,10 @@ const TOTAL_WORKING_DAYS_PER_MONTH = 20;
 const TOTAL_WORKING_HOURS_PER_MONTH = 100;
 
 var calculateDailyWage = 0;
-var workigHoursCompleted = 0;
+var workingHoursCompleted = 0;
 var numberOfHalfDays = 0;
 var numberOfFulldays = 0;
-var trackCountNumberOFdays = 0;
+var workingDaysCompleted  = 0;
 var numberofPresent = 0;
 var numberOfAbsent = 0;
 
@@ -33,35 +33,49 @@ getWorkPerDay = (employeeStatus) => {
 }
 
 /**
+ * @description  This function compute daily wage based on empployee attendance status 
+ * and then return updated wage as per attendance
+ * It also computation of total wage based on half days and full days
+*/
+computeEmpWage = (empAttandanceStatus) => {
+    if (empAttandanceStatus == IS_PRESENT) {
+        numberofPresent++;
+        let empDayStatus = (Math.floor(Math.random() * 10) % 2);
+        empDayStatus == 0 ? numberOfHalfDays++ : numberOfFulldays++
+
+        dayHours = getWorkPerDay(empDayStatus);
+        workingHoursCompleted = workingHoursCompleted + dayHours;
+
+        workingHoursCompleted > 100 ? workingHoursCompleted = 100 :  workingHoursCompleted;
+
+        calculateDailyWage = calculateDailyWage + (dayHours * WAGE_PER_HOUR);
+    } else {
+        numberOfAbsent++;
+    }
+
+    return calculateDailyWage;
+}
+
+/**
  * @description  Computaion ends based on total working days per months
  * and total working hours per months
- * Computation of total wage based on half days and full days
+ * Computation of total wage delegated to a seprate function
+ * Print the valuable results
 */
 main = () => {
     console.log("Welcome to EmployeeWage Computation System ");
 
-    while (trackCountNumberOFdays < TOTAL_WORKING_DAYS_PER_MONTH && workigHoursCompleted < TOTAL_WORKING_HOURS_PER_MONTH) {
-        let empCheck = Math.floor(Math.random() * 10) % 2;
-
-        if (empCheck == IS_PRESENT) {
-            numberofPresent++;
-            let employeeStatus = (Math.floor(Math.random() * 10) % 2);
-
-            // Counting number of full days and half days
-            employeeStatus == 0 ? numberOfHalfDays++ : numberOfFulldays++
-
-            let dayHours = getWorkPerDay(employeeStatus);
-            calculateDailyWage = calculateDailyWage + (dayHours * WAGE_PER_HOUR);
-        } else {
-            numberOfAbsent++;
-        }
-        trackCountNumberOFdays++;
+    while (workingDaysCompleted < TOTAL_WORKING_DAYS_PER_MONTH && workingHoursCompleted < TOTAL_WORKING_HOURS_PER_MONTH) {
+        let empAttandanceStatus = (Math.floor(Math.random() * 10) % 2);
+        calculateDailyWage = computeEmpWage(empAttandanceStatus);
+        workingDaysCompleted++;
     }
 
     console.log("Number of days presents  - " + numberofPresent);
     console.log("total of days absents    - " + numberOfAbsent);
     console.log("total half working days  - " + numberOfHalfDays);
     console.log("total full working days  - " + numberOfFulldays);
+    console.log("Number of hours completed- " + workingHoursCompleted);
     console.log("total wage               - " + calculateDailyWage);
 }
 
