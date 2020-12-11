a34880197fa893adbeb0b310c4ba88562af81f74
aakash2809 <aakashrajak289@gmail.com>
2020-12-11 22:09:10 +0530

    Refactor : uc6

--- EmpWageBuilder.js
+++ EmpWageBuilder.js
@@ -1,21 +1,24 @@
-const IS_PRESENT = 1; // constant to check whether present or absent
-const WAGE_PER_HOUR = 20; //wage per hour
-const FULL_DAY_HOUR = 8; //Number of fixed Hours for full day
-const PART_TIME_HOUR = 4; //Number of fixed Hours for half day
-const TOTAL_WORKING_DAYS_PER_MONTH = 20;//Total no of days in month 
-const TOTAL_WORKING_HOURS_PER_MONTH = 100;//Maximum number of working hours in month
-var calculateDailyWage = 0; //Variable to assign calculated daily wage
-var workigHoursCompleted = 0; //Number of hours completed by Employee
-var dayHours; //Number of hours work done.
-var numberOfHalfDays = 0;//variable to store number of half days
-var numberOfFulldays = 0;//variable to store number of full days
-var count = 0;//Varible to track count to validate condition
-var numberofPresent = 0;//Variable to track number of presents
-var numberOfAbsent = 0;//Variable to track number of abasents
+const IS_PRESENT = 1;
+const WAGE_PER_HOUR = 20;
+const FULL_DAY_HOUR = 8;
+const PART_TIME_HOUR = 4;
+const TOTAL_WORKING_DAYS_PER_MONTH = 20;
+const TOTAL_WORKING_HOURS_PER_MONTH = 100;
 
-//To find how many hours work done by emmployee
-function getWorkPerDay(employeeStatus) {
+var calculateDailyWage = 0;
+var workigHoursCompleted = 0;
+var numberOfHalfDays = 0;
+var numberOfFulldays = 0;
+var trackCountNumberOFdays = 0;
+var numberofPresent = 0;
+var numberOfAbsent = 0;
+
+/**
+ * @description this function will return  how many hours work done by emmployee
+*/
+getWorkPerDay = (employeeStatus) => {
     var workDone = 0;
+
     switch (employeeStatus) {
         case 0:
             workDone = FULL_DAY_HOUR;
@@ -23,44 +26,43 @@ function getWorkPerDay(employeeStatus) {
         case 1:
             workDone = PART_TIME_HOUR;
             break;
-
+        default:
+            console.log("employee status key not matched with cases");
     }
     return workDone;
 }
 
-console.log("           Welcome to EmployeeWage Computation System                 ");
-console.log("----------------------------------------------------------------------");
+/**
+ * @description  Computaion ends based on total working days per months
+ * and total working hours per months
+ * Computation of total wage based on half days and full days
+*/
+main = () => {
+    console.log("Welcome to EmployeeWage Computation System ");
 
-// loop till working days and hours per month 
-while (count < TOTAL_WORKING_DAYS_PER_MONTH && workigHoursCompleted < TOTAL_WORKING_HOURS_PER_MONTH) {
+    while (trackCountNumberOFdays < TOTAL_WORKING_DAYS_PER_MONTH && workigHoursCompleted < TOTAL_WORKING_HOURS_PER_MONTH) {
+        let empCheck = Math.floor(Math.random() * 10) % 2;
 
-    let empCheck = Math.floor(Math.random() * 10) % 2;// computation
+        if (empCheck == IS_PRESENT) {
+            numberofPresent++;
+            let employeeStatus = (Math.floor(Math.random() * 10) % 2);
 
-    if (empCheck == IS_PRESENT) {
-        numberofPresent++;
+            // Counting number of full days and half days
+            employeeStatus == 0 ? numberOfHalfDays++ : numberOfFulldays++
 
-        let employeeStatus = (Math.floor(Math.random() * 10) % 2); // key genration for half day or full
-
-        // counting number of full days and half days
-        if (employeeStatus == 0)
-            numberOfHalfDays++;
-        else
-            numberOfFulldays++;
-
-        let dayHours = getWorkPerDay(employeeStatus);
-        calculateDailyWage = calculateDailyWage + (dayHours * WAGE_PER_HOUR);
+            let dayHours = getWorkPerDay(employeeStatus);
+            calculateDailyWage = calculateDailyWage + (dayHours * WAGE_PER_HOUR);
+        } else {
+            numberOfAbsent++;
+        }
+        trackCountNumberOFdays++;
     }
 
-    else
-        numberOfAbsent++;
-    count++;
-
+    console.log("Number of days presents  - " + numberofPresent);
+    console.log("total of days absents    - " + numberOfAbsent);
+    console.log("total half working days  - " + numberOfHalfDays);
+    console.log("total full working days  - " + numberOfFulldays);
+    console.log("total wage               - " + calculateDailyWage);
 }
 
-console.log("Number of days presents  - " + numberofPresent);
-console.log("total of days absents    - " + numberOfAbsent);
-console.log("total half working days  - " + numberOfHalfDays);
-console.log("total full working days  - " + numberOfFulldays);
-console.log("total wage               - " + calculateDailyWage);
-
-
+main();
