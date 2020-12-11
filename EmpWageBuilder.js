a79ae395f3b9e06c93af44054be3e9acdc426d84
aakash2809 <aakashrajak289@gmail.com>
2020-12-11 06:14:51 +0530

    Add : Compute wage using switch case

--- EmpWageBuilder.js
+++ EmpWageBuilder.js
@@ -1,25 +1,43 @@
 
-var IS_PRESENT = 1; //default present status
-var WAGE_PER_HOUR = 20; //wage per hour
-var FULL_DAY_HOUR = 8; //Number of fixed Hours for full day
-var PART_TIME_HOUR = 4; //Number of fixed Hours for half day
-var calculateDailyWage = 0;
+const IS_PRESENT = 1; //default present status
+const WAGE_PER_HOUR = 20; //wage per hour
+const FULL_DAY_HOUR = 8; //Number of fixed Hours for full day
+const PART_TIME_HOUR = 4; //Number of fixed Hours for half day
+var calculateDailyWage = 0; //Variable to assign calculated daily wage
+var employeeStatus; //Variable to assign random genrated key for half day or full day
+var dayHours; //Number of hours work done.
+
+//To find how many hours work done by emmployee
+function getWorkPerDay(employeeStatus) {
+    var workDone = 0;
+    switch (employeeStatus) {
+        case 0:
+            workDone = FULL_DAY_HOUR;
+            break;
+        case 1:
+            workDone = PART_TIME_HOUR;
+            break;
+
+    }
+    return workDone;
+}
 
 console.log("           Welcome to EmployeeWage Computation System                 ");
 console.log("----------------------------------------------------------------------");
 
-var empCheck = Math.floor(Math.random() * 10) % 2; //Employee status computation
+var empAttendanceCheck = Math.floor(Math.random() * 10) % 2; //Employee status computation
 
-if (empCheck == IS_PRESENT) {
+if (empAttendanceCheck == IS_PRESENT) {
     console.log("Employee is Present");
 } else {
     console.log("Employee is not Present");
 }
 
-calculateDailyWage = (FULL_DAY_HOUR * WAGE_PER_HOUR); //daily full day wage camputation
-console.log("Employee daily Full day wage - " + calculateDailyWage);
+employeeStatus = (Math.floor(Math.random() * 10) % 2); // key genration for half day or full day
+dayHours = getWorkPerDay(employeeStatus);
 
-calculateDailyWage = 0;                             // assigning zero for computation  wage for part time
-calculateDailyWage = (PART_TIME_HOUR * WAGE_PER_HOUR); //daily part time wage camputation
-console.log("Employee daily Part time day wage - " + calculateDailyWage);
+calculateDailyWage = (dayHours * WAGE_PER_HOUR); // daily full day wage camputation
+console.log("Employee daily  day wage according to full day or half day - " + calculateDailyWage);
+
+
 
