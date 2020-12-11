65f79abddee7289fa11ebcf3ab7e4f2c848f5864
aakash2809 <aakashrajak289@gmail.com>
2020-12-11 06:06:47 +0530

    -

--- EmpWageBuilder.js
+++ EmpWageBuilder.js
@@ -3,7 +3,26 @@ const IS_PRESENT = 1; //default present status
 const WAGE_PER_HOUR = 20; //wage per hour
 const FULL_DAY_HOUR = 8; //Number of fixed Hours for full day
 const PART_TIME_HOUR = 4; //Number of fixed Hours for half day
-var calculateDailyWage = 0;
+var calculateDailyWage = 0; //Variable to assign calculated daily wage
+var employeeStatus; //Variable to assign random genration key for half day or full
+var dayHours;
+
+//To find how many hours work done by emmployee
+function getWorkPerDay(employeeStatus) {
+    let workDone = 0;
+    switch (employeeStatus) {
+        case 0:
+            workDone = FULL_DAY_HOUR;
+            break;
+        case 1:
+            workDone = PART_TIME_HOUR;
+            break;
+
+    }
+    return workDone;
+}
+
+
 
 console.log("           Welcome to EmployeeWage Computation System                 ");
 console.log("----------------------------------------------------------------------");
@@ -15,6 +34,14 @@ if (empAttendanceCheck == IS_PRESENT) {
 } else {
     console.log("Employee is not Present");
 }
+
+
+employeeStatus = (Math.floor(Math.random() * 10) % 2); // key genration for half day or full day
+dayHours = getWorkPerDay(employeeStatus);
+
+
+
+
 
 calculateDailyWage = (FULL_DAY_HOUR * WAGE_PER_HOUR); //daily full day wage camputation
 console.log("Employee daily Full day wage - " + calculateDailyWage);
